f8ff1c7c3e7a1d9ca1e2a4902ec8ddd42c4de2aa
Ezhkov83 <63504755+Ezhkov83@users.noreply.github.com>
2021-05-28 10:56:44 +0300

    Add files via upload

---
+++ Examen-main/exam-master/src/Auto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src
+{
+    class Auto
+    {
+        string power= "";
+        string model = "";
+        int price = 0;
+
+        public string Power
+        {
+            get { return this.power; }
+            set { this.power = value; }
+        }
+
+        public string Model
+        {
+            get { return this.model; }
+            set { this.model = value; }
+        }
+
+        public int Price
+        {
+            get { return this.price; }
+            set { this.price = value; }
+        }
+
+    }
+}
